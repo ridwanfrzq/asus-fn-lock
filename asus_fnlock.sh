15eb558891ad70f93c3d63afe83ce351dad41a7d
Muhammad Ridwan Farizqi <mridwan6661@gmail.com>
2025-11-15 19:09:27 +0700

    add mkinitcpio

--- asus_fnlock.sh
+++ asus_fnlock.sh
@@ -6,16 +6,18 @@ if [[ -f "$FN_LOCK" ]]; then
     FN_LOCK_STATE=$(cat "$FN_LOCK")
 
     if [[ "$FN_LOCK_STATE" == "Y" ]]; then
-        read -p "FN Lock is enabled on startup, disable (Y/n): " input
+        read -p "fn lock is enabled on startup, disable (Y/n): " input
         if [[ "$input" == "Y" || "$input" == "y" ]]; then
             echo "options asus_wmi fnlock_default=N" | sudo tee /etc/modprobe.d/asus_fnlock.conf > /dev/null
-            echo "Success.. Fnlock now disabled"
+			sudo mkinitcpio -P
+            echo "Success.. fn lock now disabled"
         fi
     else
-        read -p "FN Lock is disabled on startup, enable (Y/n): " input
+        read -p "fn lock is disabled on startup, enable (Y/n): " input
         if [[ "$input" == "Y" || "$input" == "y" ]]; then
             echo "options asus_wmi fnlock_default=Y" | sudo tee /etc/modprobe.d/asus_fnlock.conf > /dev/null
-            echo "Success.. Fnlock now enabled"
+			sudo mkinitcpio -P
+            echo "Success.. fn lock now enabled"
         fi
     fi
 else
